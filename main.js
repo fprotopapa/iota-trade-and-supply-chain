e0e5631cb9ff6c3800ce8d6430e5a8f8e65b1c9e
Fabbio Protopapa <fprotopapa.git@gmail.com>
2021-12-02 22:26:31 +0100

    Enable communcication via rest

--- main.js
+++ main.js
@@ -9,17 +9,14 @@
 *        - Store or retrieve index to work with reloaded instances
 *        - Multi Branch
 */
-//const streams = require('./node/streams');
-//streams.set_panic_hook();
-
 var util = require('./utils');
 var author = require('./mbAuthor');
 var subs = require('./mbSubscriber');
 var api = require('./server');
 
-const DEBUG_SKIP_STREAMS = true;
+const DEBUG_SKIP_STREAMS = false;
+const DEBUG_SKIP_SENDING = false;
 
-const port = 8000;
 
 async function main() {
   /*
@@ -28,42 +25,10 @@ async function main() {
     author -> announces channel
 
   */
- // Make announcement link avaiable
-  api.updateAnnouncementLink('1234456');
-  // Start rest api
-  rest = api.createAPI();
-  rest.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`)
-  });
-  // Verify author
-  let authorDid = subs.getAuthorDID('localhost', port, protocol='http');
-  authorDid.then(function(result) {
-    console.log('Author DID: ' + result.body);
-    if (util.verifyDID(result.body)) {
-      console.log('Author verified');
-    } else {
-      console.log('Not the author');
-    }
-  });
-  // Get announcement link
-  let authorAnn = subs.getAnnouncementLink('localhost', port, protocol='http');
-  authorAnn.then(function(result) {
-    console.log('Ann Link recv: ' + result.body);
-  });
-  // client send subscribtion link to author
-  let data = subs.makeSubLinkJson('1312312', '34634', 'hello');
-  let code = subs.sendSubscribtionLink('localhost', port, data, protocol='http')
-  code.then(function(result) {
-    console.log('Sub link posted: ' + result.statusCode);
-  });
-  // Get subscribed clients
-  var actSubs = {};
-  while(Object.keys(actSubs).length === 0) {
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    subscribers = api.getSubscribers();
-    actSubs = JSON.parse(JSON.stringify(subscribers));
-  }
-  console.log('Subscribers:' + JSON.stringify(actSubs));
+  // Configure rest api
+  const restUrl = util.getRestURL();
+  const protocol = 'http';
+  const port = 8000;
   /* 
   *   Streams 
   *
@@ -74,6 +39,13 @@ async function main() {
     // Create channel/ Open channel
     let auth = await author.makeAuthor(client);
     let announcementLink = author.parseAnnouncementLink(auth);
+    // Make announcement link avaiable
+    api.updateAnnouncementLink(announcementLink);
+    // Start rest api
+    rest = api.createAPI();
+    rest.listen(port, () => {
+      console.log(`Server listening at ${protocol}://${restUrl}:${port}`)
+    });
     // Log Channel details
     author.logChannel(auth);
     // Log node details
@@ -88,21 +60,61 @@ async function main() {
     nodeUrl = util.getNodeURL();
     subA = subs.generateNewSubscriber(nodeUrl, util.makeSeed(81));
     subB = subs.generateNewSubscriber(nodeUrl, util.makeSeed(81));
-    await subs.receiveAnnouncement(announcementLink, subA);
-    await subs.receiveAnnouncement(announcementLink, subB);
-    
-    subLinkA = await subs.subscripeToChannel(announcementLink, subA);
-    subLinkB = await subs.subscripeToChannel(announcementLink, subB);
+    // Verify author
+    let authorDid = subs.getAuthorDID(restUrl, port, protocol);
+    authorDid.then(function(result) {
+      console.log('Author DID: ' + result.body);
+      if (util.verifyDID(result.body)) {
+        console.log('Author verified');
+      } else {
+        console.log('Not the author');
+      }
+    });
+    // Get announcement link
+    let restAnnLink;
+    let restAnnLinkStr = subs.getAnnouncementLink(restUrl, port, protocol);
+    await restAnnLinkStr.then(function(result) {
+      let str = JSON.stringify(result.body).replace('"', '').replace(/"$/,'');
+      restAnnLink = util.parseAnnouncementLinkString(str);
+      console.log('Ann Link recv: ' + str);
+    });
+    // Receive and subscribe to channel
+    await subs.receiveAnnouncement(restAnnLink, subA);
+    await subs.receiveAnnouncement(restAnnLink, subB);
+    let subLinkA = await subs.subscripeToChannel(restAnnLink, subA);
+    let subLinkB = await subs.subscripeToChannel(restAnnLink, subB);
     /*
 
       Author receives subscribtions & sends out keyload (needed to attach messages)
 
       Subscriber -> receives annnouncement -> subscribes to channel
     */
-    await author.receiveSubscription(subLinkA, auth);
-    await author.receiveSubscription(subLinkB, auth);
+    // Client send subscribtion link to author (Sub Link, DID, Name)
+    let subDataA = subs.makeSubLinkJson(subLinkA.toString(), '111111111', 'SubA');
+    let responseA = subs.sendSubscribtionLink(restUrl, port, subDataA, protocol)
+    responseA.then(function(result) {
+      console.log('SubA link posted');
+    });
+    let subDataB = subs.makeSubLinkJson(subLinkB.toString(), '222222222', 'SubB');
+    let responseB = subs.sendSubscribtionLink(restUrl, port, subDataB, protocol)
+    responseB.then(function(result) {
+      console.log('SubB link posted');
+    });
+    // Get subscribed clients
+    var actSubs = {};
+    while(Object.keys(actSubs).length === 0) {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      subscribers = api.getSubscribers();
+      actSubs = JSON.parse(JSON.stringify(subscribers));
+    }
+    console.log('Subscribers:' + JSON.stringify(actSubs));
+    // Register subscribers
+    let restSubLinkA = util.parseAnnouncementLinkString(actSubs["SubA"]["subLink"]);
+    let restSubLinkB = util.parseAnnouncementLinkString(actSubs["SubB"]["subLink"]);
+    await author.receiveSubscription(restSubLinkA, auth);
+    await author.receiveSubscription(restSubLinkB, auth);
     console.log("Subscription processed");
-
+    // Distributing keyload
     console.log("Sending Keyload");
     response = await auth.clone().send_keyload_for_everyone(announcementLink.copy());
     let keyload_link = response.link;
@@ -115,29 +127,31 @@ async function main() {
       Author -> synch state -> build payload in bytes ->
       sends messages and attaches to link (single branch: attach to last message)
     */
-    await util.syncState(auth);
+    if (!DEBUG_SKIP_SENDING) {
+      await util.syncState(auth);
 
-    let public_payload = util.toBytes("Public");
-    let masked_payload = util.toBytes("Masked");
+      let public_payload = util.toBytes("Public");
+      let masked_payload = util.toBytes("Masked");
 
-    console.log("Author Sending multiple signed packets");
-    let msgLink = keyload_link;
-    for (var x = 0; x < 3; x++) {
-      msgLink = await util.sendSignedPacket(msgLink, auth, public_payload, masked_payload);
-      console.log("Signed packet at: ", msgLink.toString());
-      console.log("Signed packet index: " + msgLink.toMsgIndexHex());
+      console.log("Author Sending multiple signed packets");
+      let msgLink = keyload_link;
+      for (var x = 0; x < 3; x++) {
+        msgLink = await util.sendSignedPacket(msgLink, auth, public_payload, masked_payload);
+        console.log("Signed packet at: ", msgLink.toString());
+        console.log("Signed packet index: " + msgLink.toMsgIndexHex());
+      }
+      /*
+
+          Subscriber receives messages
+
+          Subscriber -> fetch messages
+      */
+      console.log("\Subscriber fetching next messages");
+      let messagesA = await util.fetchNextMessages(subA);
+      util.showMessages(messagesA, "SubA");
+      let messagesB = await util.fetchNextMessages(subB);
+      util.showMessages(messagesB, "SubB");
     }
-    /*
-
-        Subscriber receives messages
-
-        Subscriber -> fetch messages
-    */
-    console.log("\Subscriber fetching next messages");
-    let messagesA = await util.fetchNextMessages(subA);
-    util.showMessages(messagesA, "SubA");
-    let messagesB = await util.fetchNextMessages(subB);
-    util.showMessages(messagesB, "SubB");
   }
 }
 
